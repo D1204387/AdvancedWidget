8f62883bd22a60d78b47b6724d4fe0cd3ee8f3a4
陳憶柔 <D1204387@o365.fcu.edu.tw>
2024-08-07 12:08:38 +0800

    透過客製化Adapter與樣式的xml的方式,讓ListView的內容能自動變化

--- ListViewTest/app/src/main/java/com/example/listviewtest/MainActivity.java
+++ ListViewTest/app/src/main/java/com/example/listviewtest/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.listviewtest;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -9,6 +12,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -22,6 +26,35 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
     private ListView cityListView;
     private List<String> cities = new ArrayList<>();
+
+    private class CityAdapter extends ArrayAdapter<City>{
+
+        public CityAdapter(@NonNull Context context, ArrayList<City> cities) {
+            super(context, 0, cities);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent){
+            // 找到哪一筆資料
+            City city = getItem(position);
+
+            // 設定要塞入的樣式
+            if(convertView == null){
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.city_information,parent,false);
+
+            }
+            // 將資料塞入設計好的樣式
+            TextView tvName = (TextView) convertView.findViewById(R.id.cityName);
+            TextView tvZipCode = (TextView) convertView.findViewById(R.id.zipCode);
+
+            tvName.setText(city.name);
+            tvZipCode.setText(String.valueOf(city.zipCode));
+
+            return convertView;
+
+
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,13 +74,21 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     }
 
     public void setCities(){
-        cities = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.cities)));
-        cities.add("花蓮縣");
-        cities.add("彰化縣");
-        cities.add("澎湖縣");
-        cities.add("新竹市");
-        // new ArrayAdapter
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,cities);
+//        cities = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.cities)));
+//        cities.add("花蓮縣");
+//        cities.add("彰化縣");
+//        cities.add("澎湖縣");
+//        cities.add("新竹市");
+//        // new ArrayAdapter
+//        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,cities);
+//        cityListView.setAdapter(adapter);
+
+        ArrayList<City> cityList = new ArrayList<City>();
+        cityList.add(new City("台北市",100));
+        cityList.add(new City("新北市",207));
+        cityList.add(new City("台中市",404));
+        CityAdapter adapter = new CityAdapter(this,cityList);
+        ListView cityListView = (ListView)findViewById(R.id.cityListView);
         cityListView.setAdapter(adapter);
     }
 
